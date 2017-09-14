7aedeece49c99fb2cf8f24ec2aa6564addd6e37b
renuka <renutarse1@gmail.com>
2017-09-14 17:09:40 +0530

    stringbuffer commit

---
+++ stringbuffer/src/stringbuffer/BufferTest.java
@@ -0,0 +1,15 @@
+package stringbuffer;
+
+public class BufferTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 StringBuffer buffer=new StringBuffer("hello");  
+		 	        buffer.append("java");  
+		 	        System.out.println(buffer);  
+		     } 
+
+
+	}
+
+
